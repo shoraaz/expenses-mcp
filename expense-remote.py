455b409196d03b0c4046e89d61cbeb4bc10af4be
shoraaz <shouryaaar@mail.com>
2025-10-20 00:20:40 +0530

    read only fix

--- expense-remote.py
+++ expense-remote.py
@@ -158,4 +158,4 @@ async def categories() -> str:
 
 # Start the server
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0"port=8000)
+    mcp.run(transport="http", host="0.0.0.0",port=8000)
